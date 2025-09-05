0f2d9704e8feb9e707bdb9d2044a75d1aa1be8b3
LeonardoBlanco <rm99119@fiap.com.br>
2025-09-04 21:04:04 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hospitech/HospitechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hospitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitechApplication.class, args);
+	}
+
+}
